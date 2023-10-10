0b542c88cad14a1ffb418f923ae4313ba8e3c5a5
Mathias Karstädt <mathias.karstaedt@gmail.com>
2023-10-10 14:17:49 +0200

    adds nvim_lazy configuration files

---
+++ nvim_lazy/.config/nvim_lazy/lua/plugins/tmux.lua
@@ -0,0 +1,12 @@
+return {
+  -- tmux vim
+  {
+    "christoomey/vim-tmux-navigator",
+    keys = {
+      { "<C-h>", "<cmd>TmuxNavigateLeft<cr>" },
+      { "<C-j>", "<cmd>TmuxNavigateDown<cr>" },
+      { "<C-k>", "<cmd>TmuxNavigateUp<cr>" },
+      { "<C-l>", "<cmd>TmuxNavigateRight<cr>" },
+    },
+  },
+}
