b39e20c6259a53429f256caad91852c078914dbb
Mathias Karstädt <mathias.karstaedt@gmail.com>
2024-02-06 18:05:11 +0100

    adds chatgpt to nvim_lazy

---
+++ nvim_lazy/.config/nvim_lazy/lua/plugins/chatgpt.lua
@@ -0,0 +1,17 @@
+return {
+  -- chatgpt
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup({
+        api_key_cmd = "op read op://private/OpenAI/credential --no-newline",
+      })
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+  },
+}
